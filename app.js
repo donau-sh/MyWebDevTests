7661a1da0b49d42b904217356dece4c96d0ec1a1
donau-sh <donau-sh@users.noreply.github.com>
2022-01-25 19:29:05 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require("express");
-const https = require("https");
+const bodyParser = require("body-parser");
+const request = require("request");
 
-const app = new express();
+const app = express();
 
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=frankfurt&appid=b1634becb61ee9ed1dbed769bef3b56e&units=metric";
-    https.get(url, function(response) {
-        console.log(response.stattus);
-
-        response.on("data", function(data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const desp = weatherData.weather[0].description;
-            console.log(weatherData);
-            console.log(temp);
-            console.log(desp);
-        })
-    })
-
-    res.send("Server is up and running.");
-});
-
-
+    res.sendFile(__dirname + "/signup.html");
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
-});
+})
